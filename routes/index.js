d40ea52b9d878f656672b1c44f86627250d3b23c
jlvivero <josluivivgar@gmail.com>
2017-01-13 16:55:06 -0800

    remove random comments

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/*
-var link = require('./link');
-var max_likes = require('./likes');
-var session = require('./createSession');
-*/
 var add = require('./add');
 var show = require('./show');
 
 router.get('/show', show);
 router.post('/add', add);
-/*
-router.get('/facebook', function(req,res){
-  res.send("this will return the request parameteres that you can do to facebook and will eventually have a post that will be able to select which parameteres you want");
-});
-router.post('/facebook/likes', max_likes);
-router.get('/facebook/link', link);
-router.post('/facebook/newsession', session);
-*/
+
 module.exports = router;
